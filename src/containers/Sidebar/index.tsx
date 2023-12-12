3e528919985b01240bb2c54606e16d24bb02049e
Iago Valverde <iagopachiani96@gmail.com>
2023-12-12 14:14:45 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/iagovalverde.png" />
-    <Titulo fontSize={20}>Iago Valverde</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>I. Valverde</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        iagovalverde
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack Python
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
